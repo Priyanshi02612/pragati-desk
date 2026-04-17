d77881daaa883899d25f228c8a837532300fa87a
Priyanshi <piyuklsrya26@gmail.com>
2026-04-17 14:36:46 +0530

    Minor change

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,9 +26,10 @@ const ensureAppReady = async () => {
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   }),
 );
 
